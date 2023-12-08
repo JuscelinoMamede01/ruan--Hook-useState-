d110c7457cdd8f0e793f5f65174cf02699fbd6e5
JuscelinoMamede <juscemamede01@gmail.com>
2023-12-08 12:43:24 -0300

    continuação da av01

---
+++ src/app/components/card/Card.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { useState } from "react";
+import { CardData } from "./CardData";
+
+interface Card {
+  id: number;
+  expression: string;
+  translation: string;
+  tip: string;
+  example1: string;
+  example2: string;
+}
+
+export const Card = () => {
+  const [flippedCard, setFlippedCard] = useState<Card | null>(null);
+
+  const handleCardFlip = (card: Card) => {
+    setFlippedCard((prevCard) => (prevCard?.id === card.id ? null : card));
+  };
+
+  return (
+    <div className="w-full bg-gray-900 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-4 p-10 rounded">
+      {CardData.map((card: Card) => (
+        <div key={card.id} className="relative">
+          {/* front */}
+          <div
+            className={`min-w-[250px] h-96 bg-slate-300 rounded flex flex-col justify-center items-center p-4 transform ${
+              flippedCard?.id === card.id ? "rotate-y-180" : ""
+            }`}
+          >
+            <h3 className="text-gray-800 text-lg font-bold mb-2">
+              {card.expression}
+            </h3>
+            <button
+              onClick={() => handleCardFlip(card)}
+              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+            >
+              Learn
+            </button>
+          </div>
+          {/* back */}
+          <div
+            key={card.id}
+            className={`absolute inset-0 min-w-[250px] bg-yellow-300 rounded flex justify-center items-center flex-col p-4 transform rotate-y-180 backface-visibility-hidden ${
+              flippedCard?.id === card.id ? "" : "hidden"
+            }`}
+          >
+            <div className="border-solid border-2 border-black rounded px-4 text-left ">
+              <p>
+                <span className="font-bold">Tradução:</span> {card.translation}
+              </p>
+              <p>
+                <span className="font-bold">Dica:</span> {card.tip}
+              </p>
+              <p>
+                <span className="font-bold">Ex1:</span> {card.example1}
+              </p>
+              <p>
+                <span className="font-bold">Ex2:</span> {card.example2}
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
---
+++ src/app/components/card/CardData.tsx
@@ -0,0 +1,199 @@
+export const CardData = [
+  {
+    id: 1,
+    expression: "Break the ice",
+    translation: "Quebrar o gelo",
+    tip: "Usado para descrever a ação de iniciar uma conversa em uma situação social desconfortável.",
+    example1:
+      "It's always awkward at the beginning of a party, but telling a joke can really help to break the ice.",
+    example2:
+      "When meeting new colleagues, a friendly smile can be the best way to break the ice.",
+  },
+  {
+    id: 2,
+    expression: "Hit the hay",
+    translation: "Ir para a cama",
+    tip: "Uma maneira informal de dizer que está indo dormir.",
+    example1:
+      "I have an early meeting tomorrow, so I need to hit the hay early tonight.",
+    example2:
+      "After a long day of work, I'm ready to hit the hay and get a good night's sleep.",
+  },
+  {
+    id: 3,
+    expression: "Piece of cake",
+    translation: "Mamão com açúcar",
+    tip: "Significa que algo é muito fácil de fazer.",
+    example1:
+      "Don't worry about the exam; for her, solving math problems is a piece of cake.",
+    example2:
+      "Cooking spaghetti is a piece of cake – just boil water and add the pasta.",
+  },
+  {
+    id: 4,
+    expression: "Let the cat out of the bag",
+    translation: "Contar o segredo",
+    tip: "Revelar um segredo ou informação confidencial.",
+    example1:
+      "I accidentally let the cat out of the bag about the surprise party.",
+    example2:
+      "We were supposed to keep the project a secret, but someone let the cat out of the bag.",
+  },
+  {
+    id: 5,
+    expression: "Cost an arm and a leg",
+    translation: "Custar os olhos da cara",
+    tip: "Algo muito caro ou custoso.",
+    example1:
+      "Buying a new car can cost an arm and a leg, but it's worth it for the comfort.",
+    example2:
+      "Traveling during the holiday season can cost an arm and a leg due to high ticket prices.",
+  },
+  {
+    id: 6,
+    expression: "Burn the midnight oil",
+    translation: "Queimar a meia-noite",
+    tip: "Trabalhar até tarde da noite em um projeto.",
+    example1:
+      "To finish the project on time, they had to burn the midnight oil for several nights.",
+    example2:
+      "During exam week, students often burn the midnight oil studying late into the night.",
+  },
+  {
+    id: 7,
+    expression: "Kick the bucket",
+    translation: "Bater as botas",
+    tip: "Uma maneira informal de dizer que alguém morreu.",
+    example1:
+      "He was so old that we knew he would kick the bucket soon, but it was still sad when it happened.",
+    example2:
+      "I don't want to kick the bucket without traveling to my dream destinations.",
+  },
+  {
+    id: 8,
+    expression: "It's a piece of cake",
+    translation: "É moleza",
+    tip: "Usado para dizer que algo é fácil.",
+    example1:
+      "Don't worry about fixing the computer; it's a piece of cake for our IT team.",
+    example2:
+      "For her, learning to play the piano is a piece of cake because she's so musically talented.",
+  },
+  {
+    id: 9,
+    expression: "Bite the bullet",
+    translation: "Engolir o sapo",
+    tip: "Aceitar uma situação desagradável.",
+    example1:
+      "Losing the game was tough, but we had to bite the bullet and congratulate the winning team.",
+    example2:
+      "When faced with a difficult decision, sometimes you just have to bite the bullet and make a choice.",
+  },
+  {
+    id: 10,
+    expression: "Spill the beans",
+    translation: "Contar tudo",
+    tip: "Revelar informações secretas.",
+    example1: "I can't keep a secret; I always end up spilling the beans.",
+    example2:
+      "Before the surprise announcement, she accidentally spilled the beans about the upcoming event.",
+  },
+  {
+    id: 11,
+    expression: "Hit the nail on the head",
+    translation: "Acertar em cheio",
+    tip: "Descreve alguém que entendeu completamente uma situação.",
+    example1:
+      "During the discussion, she really hit the nail on the head and identified the main issue.",
+    example2:
+      "Your analysis hit the nail on the head – that's exactly what went wrong with the project.",
+  },
+  {
+    id: 12,
+    expression: "Burn bridges",
+    translation: "Queimar pontes",
+    tip: "Tomar ações que impossibilitam um retorno positivo.",
+    example1:
+      "Quitting the job without notice is a sure way to burn bridges with your employer.",
+    example2:
+      "Be careful not to burn bridges in your personal relationships; you never know when you might need someone's help.",
+  },
+  {
+    id: 13,
+    expression: "Jump on the bandwagon",
+    translation: "Entrar na onda",
+    tip: "Adotar uma tendência popular ou comportamento.",
+    example1:
+      "After the success of the new technology, many companies decided to jump on the bandwagon and incorporate it into their products.",
+    example2:
+      "Don't just jump on the bandwagon because everyone else is doing it; consider the consequences.",
+  },
+  {
+    id: 14,
+    expression: "Break a leg",
+    translation: "Boa sorte!",
+    tip: "Desejar boa sorte, especialmente antes de uma apresentação.",
+    example1:
+      "Before going on stage, the director told the actors, 'Break a leg!' to wish them luck.",
+    example2: "I have an important interview tomorrow; I hope I break a leg!",
+  },
+  {
+    id: 15,
+    expression: "A piece of the pie",
+    translation: "Uma parte do bolo",
+    tip: "Ter uma parte de um benefício ou lucro.",
+    example1:
+      "Investors are eager to get a piece of the pie in the booming tech industry.",
+    example2:
+      "In a successful partnership, both parties get a fair piece of the pie.",
+  },
+  {
+    id: 16,
+    expression: "Beat around the bush",
+    translation: "Enrolar",
+    tip: "Evitar falar diretamente sobre um assunto.",
+    example1:
+      "Stop beating around the bush and tell me what you really think about the proposal.",
+    example2:
+      "Instead of beating around the bush, be straightforward in your communication to avoid misunderstandings.",
+  },
+  {
+    id: 17,
+    expression: "Cry over spilled milk",
+    translation: "Chorar sobre o leite derramado",
+    tip: "Lamentar por algo que já aconteceu e não pode ser mudado.",
+    example1:
+      "I know you made a mistake, but there's no use crying over spilled milk; let's find a solution.",
+    example2:
+      "After missing the train, she realized there was no point in crying over spilled milk and decided to wait for the next one.",
+  },
+  {
+    id: 18,
+    expression: "Burn the candle at both ends",
+    translation: "Queimar a vela nos dois lados",
+    tip: "Trabalhar muito ou esgotar-se em ambas as extremidades.",
+    example1:
+      "Working two jobs and attending night classes, she was burning the candle at both ends to support her family.",
+    example2:
+      "It's not sustainable to burn the candle at both ends for an extended period; eventually, it takes a toll on your health.",
+  },
+  {
+    id: 19,
+    expression: "All ears",
+    translation: "Estar com a pulga atrás da orelha",
+    tip: "Prestar total atenção ou estar curioso sobre algo.",
+    example1: "Tell me your story; I'm all ears and ready to listen.",
+    example2:
+      "When the teacher mentioned a surprise, the students were all ears, eager to know what it was.",
+  },
+  {
+    id: 20,
+    expression: "Hit the jackpot",
+    translation: "Ganhar na loteria",
+    tip: "Ter um grande sucesso ou sorte inesperada.",
+    example1:
+      "After years of hard work, she hit the jackpot when her novel became a bestseller.",
+    example2:
+      "Finding a rare antique at the flea market felt like hitting the jackpot for the collector.",
+  },
+];
