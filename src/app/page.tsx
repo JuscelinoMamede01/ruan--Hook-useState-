4ddf30eadbf7511d08958331876f526319f7932e
JuscelinoMamede <juscemamede01@gmail.com>
2023-12-09 10:29:40 -0300

    ajuste title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { Card } from "./components/card/Card";
 export default function Home() {
   return (
     <div className="text-center p-10 ">
-      <h1 className="font-bold text-2xl">English expression</h1>
+      <h1 className=" h-20 mb-4 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 md:text-5xl lg:text-6xl">
+        English expression
+      </h1>
       <div className="flex justify-center items-center py-20">
         <Card />
       </div>
